e969c427b726d11ee7e5758e9096e00c9571f06d
PKD Biduyt <das1907118@stud.kuet.ac.bd>
2023-03-12 17:04:09 +0600

    edited java class

--- SimpleGetterAndSetter.java
+++ SimpleGetterAndSetter.java
@@ -1,11 +1,52 @@
-public class SimpleGetterAndSetter {
-    private int number;
- 
-    public int getNumber() {
-        return this.number;
-    }
- 
-    public void setNumber(int num) {
-        this.number = num;
-    }
+
+
+public class Employee {
+
+	private int id;
+	private String name;
+	
+	public Employee() {
+		this("bikash", 999);
+		System.out.println("Default Employee Created");
+	}
+	
+	public Employee(int i) {
+		this("bikash", i);
+		System.out.println("Employee Created with Default Name");
+	}
+	public Employee(String s, int i) {
+		this.id = i;
+		this.name = s;
+		System.out.println("Employee Created");
+	}
+	public static void main(String[] args) {
+
+		Employee emp = new Employee();
+		System.out.println(emp);
+		Employee emp1 = new Employee(10);
+		System.out.println(emp1);
+		Employee emp2 = new Employee("Pankaj", 20);
+		System.out.println(emp2);
+	}
+
+	@Override
+	public String toString() {
+		return "ID = "+id+", Name = "+name;
+	}
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
